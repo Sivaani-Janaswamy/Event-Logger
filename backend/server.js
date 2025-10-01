ae59277d0eecc97372028fa142007620c8f87ee7
Sivaani J <sivaanijanaswamy@gmail.com>
2025-10-01 10:16:16 +0530

    Add event.routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
+
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import eventRoutes from './routes/event.route.js';
 
 const app = express();
 const PORT = 3000;
 
 dotenv.config();
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use("/api/events",eventRoutes)
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
