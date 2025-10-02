992a291cebd43c071029d4fe0af80939ad0f2830
Sivaani J <sivaanijanaswamy@gmail.com>
2025-10-02 10:59:13 +0530

    Change package.json

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,24 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import eventRoutes from './routes/event.route.js';
 
+import path from 'path';
+const __dirname = path.resolve();
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 app.use("/api/events",eventRoutes)
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"frontend/dist")));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
-  }); 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
